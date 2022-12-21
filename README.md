7d5bec71088c47fe417e2ede6f1e39ef1b2c3ff3
linch90 <zlchu@outlook.com>
2022-12-21 14:14:34 +0800

    add V3_FLAT_CONTAINER input.

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # ✨ Publish NuGet
+
 GitHub action to build, pack & publish nuget packages automatically when a project version is updated
 
 ## Usage
+
 Create new `.github/workflows/publish.yml` file:
 
 ```yml
@@ -29,16 +31,16 @@ jobs:
         with:
           # Filepath of the project to be packaged, relative to root of repository
           PROJECT_FILE_PATH: Core/Core.csproj
-          
+
           # NuGet package id, used for version detection & defaults to project name
           # PACKAGE_NAME: Core
-          
+
           # Filepath with version info, relative to root of repository & defaults to PROJECT_FILE_PATH
           # VERSION_FILE_PATH: Directory.Build.props
 
           # Regex pattern to extract version info in a capturing group
           # VERSION_REGEX: ^\s*<Version>(.*)<\/Version>\s*$
-          
+
           # Useful with external providers like Nerdbank.GitVersioning, ignores VERSION_FILE_PATH & VERSION_REGEX
           # VERSION_STATIC: 1.0.0
 
@@ -54,6 +56,10 @@ jobs:
           # NuGet server uri hosting the packages, defaults to https://api.nuget.org
           # NUGET_SOURCE: https://api.nuget.org
 
+          # The resource url segment used for fetching package content, defaults to v3-flatcontainer
+          # https://api.nuget.org/${V3_FLAT_CONTAINER}/PACKAGE_NAME/index.json
+          # V3_FLAT_CONTAINER: v3-flatcontainer
+
           # Flag to toggle pushing symbols along with nuget package to the server, disabled by default
           # INCLUDE_SYMBOLS: false
 
@@ -65,34 +71,36 @@ jobs:
 
 ## Inputs
 
-Input | Default Value | Description
---- | --- | ---
-PROJECT_FILE_PATH | | Filepath of the project to be packaged, relative to root of repository
-PACKAGE_NAME | | NuGet package id, used for version detection & defaults to project name
-VERSION_FILE_PATH | `[PROJECT_FILE_PATH]` | Filepath with version info, relative to root of repository & defaults to PROJECT_FILE_PATH
-VERSION_REGEX | `^\s*<Version>(.*)<\/Version>\s*$` | Regex pattern to extract version info in a capturing group
-VERSION_STATIC| | Useful with external providers like Nerdbank.GitVersioning, ignores VERSION_FILE_PATH & VERSION_REGEX
-TAG_COMMIT | `true` | Flag to toggle git tagging, enabled by default
-TAG_FORMAT | `v*` | Format of the git tag, `[*]` gets replaced with actual version
-NUGET_KEY | | API key to authenticate with NuGet server
-NUGET_SOURCE | `https://api.nuget.org` | NuGet server uri hosting the packages, defaults to https://api.nuget.org
-INCLUDE_SYMBOLS | `false` | Flag to toggle pushing symbols along with nuget package to the server, disabled by default
-NO_BUILD | `false` | Flag to toggle not building the project and letting pack command handle restoring & building, disabled by default
+| Input             | Default Value                      | Description                                                                                                       |
+| ----------------- | ---------------------------------- | ----------------------------------------------------------------------------------------------------------------- |
+| PROJECT_FILE_PATH |                                    | Filepath of the project to be packaged, relative to root of repository                                            |
+| PACKAGE_NAME      |                                    | NuGet package id, used for version detection & defaults to project name                                           |
+| VERSION_FILE_PATH | `[PROJECT_FILE_PATH]`              | Filepath with version info, relative to root of repository & defaults to PROJECT_FILE_PATH                        |
+| VERSION_REGEX     | `^\s*<Version>(.*)<\/Version>\s*$` | Regex pattern to extract version info in a capturing group                                                        |
+| VERSION_STATIC    |                                    | Useful with external providers like Nerdbank.GitVersioning, ignores VERSION_FILE_PATH & VERSION_REGEX             |
+| TAG_COMMIT        | `true`                             | Flag to toggle git tagging, enabled by default                                                                    |
+| TAG_FORMAT        | `v*`                               | Format of the git tag, `[*]` gets replaced with actual version                                                    |
+| NUGET_KEY         |                                    | API key to authenticate with NuGet server                                                                         |
+| NUGET_SOURCE      | `https://api.nuget.org`            | NuGet server uri hosting the packages, defaults to https://api.nuget.org                                          |
+| INCLUDE_SYMBOLS   | `false`                            | Flag to toggle pushing symbols along with nuget package to the server, disabled by default                        |
+| NO_BUILD          | `false`                            | Flag to toggle not building the project and letting pack command handle restoring & building, disabled by default |
 
 ## Outputs
 
-Output | Description
---- | ---
-VERSION | Version of the associated git tag
-PACKAGE_NAME | Name of the NuGet package generated
-PACKAGE_PATH | Path to the generated NuGet package
-SYMBOLS_PACKAGE_NAME | Name of the symbols package generated
-SYMBOLS_PACKAGE_PATH | Path to the generated symbols package
+| Output               | Description                           |
+| -------------------- | ------------------------------------- |
+| VERSION              | Version of the associated git tag     |
+| PACKAGE_NAME         | Name of the NuGet package generated   |
+| PACKAGE_PATH         | Path to the generated NuGet package   |
+| SYMBOLS_PACKAGE_NAME | Name of the symbols package generated |
+| SYMBOLS_PACKAGE_PATH | Path to the generated symbols package |
 
 **FYI:**
+
 - Outputs may or may not be set depending on the action inputs or if the action failed
-- `NUGET_SOURCE` must support `/v3-flatcontainer/PACKAGE_NAME/index.json` for version change detection to work
+- `NUGET_SOURCE` must support `/v3-flatcontainer/PACKAGE_NAME/index.json` for version change detection to work, you can override this by `V3_FLAT_CONTAINER`
 - Multiple projects can make use of steps to configure each project individually, common inputs between steps can be given as `env` for [job / workflow](https://help.github.com/en/actions/automating-your-workflow-with-github-actions/workflow-syntax-for-github-actions#env)
 
 ## License
+
 [MIT](LICENSE)
--- index.js
+++ index.js
@@ -1,154 +1,212 @@
 const os = require("os"),
-    fs = require("fs"),
-    path = require("path"),
-    https = require("https"),
-    spawnSync = require("child_process").spawnSync
+  fs = require("fs"),
+  path = require("path"),
+  https = require("https"),
+  spawnSync = require("child_process").spawnSync;
 
 class Action {
-    constructor() {
-        this.projectFile = process.env.INPUT_PROJECT_FILE_PATH
-        this.packageName = process.env.INPUT_PACKAGE_NAME || process.env.PACKAGE_NAME
-        this.versionFile = process.env.INPUT_VERSION_FILE_PATH || process.env.VERSION_FILE_PATH || this.projectFile
-        this.versionRegex = new RegExp(process.env.INPUT_VERSION_REGEX || process.env.VERSION_REGEX, "m")
-        this.version = process.env.INPUT_VERSION_STATIC || process.env.VERSION_STATIC
-        this.tagCommit = JSON.parse(process.env.INPUT_TAG_COMMIT || process.env.TAG_COMMIT)
-        this.tagFormat = process.env.INPUT_TAG_FORMAT || process.env.TAG_FORMAT
-        this.nugetKey = process.env.INPUT_NUGET_KEY || process.env.NUGET_KEY
-        this.nugetSource = process.env.INPUT_NUGET_SOURCE || process.env.NUGET_SOURCE
-        this.includeSymbols = JSON.parse(process.env.INPUT_INCLUDE_SYMBOLS || process.env.INCLUDE_SYMBOLS)
-        this.noBuild = JSON.parse(process.env.INPUT_NO_BUILD || process.env.NO_BUILD)
+  constructor() {
+    this.projectFile = process.env.INPUT_PROJECT_FILE_PATH;
+    this.packageName =
+      process.env.INPUT_PACKAGE_NAME || process.env.PACKAGE_NAME;
+    this.versionFile =
+      process.env.INPUT_VERSION_FILE_PATH ||
+      process.env.VERSION_FILE_PATH ||
+      this.projectFile;
+    this.versionRegex = new RegExp(
+      process.env.INPUT_VERSION_REGEX || process.env.VERSION_REGEX,
+      "m"
+    );
+    this.version =
+      process.env.INPUT_VERSION_STATIC || process.env.VERSION_STATIC;
+    this.tagCommit = JSON.parse(
+      process.env.INPUT_TAG_COMMIT || process.env.TAG_COMMIT
+    );
+    this.tagFormat = process.env.INPUT_TAG_FORMAT || process.env.TAG_FORMAT;
+    this.nugetKey = process.env.INPUT_NUGET_KEY || process.env.NUGET_KEY;
+    this.nugetSource =
+      process.env.INPUT_NUGET_SOURCE || process.env.NUGET_SOURCE;
+    this.nugetV3FlatContainer =
+      process.env.INPUT_V3_FLAT_CONTAINER ||
+      process.env.V3_FLAT_CONTAINER ||
+      "v3-flatcontainer";
+    this.includeSymbols = JSON.parse(
+      process.env.INPUT_INCLUDE_SYMBOLS || process.env.INCLUDE_SYMBOLS
+    );
+    this.noBuild = JSON.parse(
+      process.env.INPUT_NO_BUILD || process.env.NO_BUILD
+    );
+  }
+
+  _printErrorAndExit(msg) {
+    console.log(`##[error]😭 ${msg}`);
+    throw new Error(msg);
+  }
+
+  _executeCommand(cmd, options) {
+    console.log(`executing: [${cmd}]`);
+
+    const INPUT = cmd.split(" "),
+      TOOL = INPUT[0],
+      ARGS = INPUT.slice(1);
+    return spawnSync(TOOL, ARGS, options);
+  }
+
+  _executeInProcess(cmd) {
+    this._executeCommand(cmd, {
+      encoding: "utf-8",
+      stdio: [process.stdin, process.stdout, process.stderr],
+    });
+  }
+
+  _tagCommit(version) {
+    const TAG = this.tagFormat.replace("*", version);
+
+    console.log(`✨ creating new tag ${TAG}`);
+
+    this._executeInProcess(`git tag ${TAG}`);
+    this._executeInProcess(`git push origin ${TAG}`);
+
+    process.stdout.write(`::set-output name=VERSION::${TAG}` + os.EOL);
+  }
+
+  _pushPackage(version, name) {
+    console.log(`✨ found new version (${version}) of ${name}`);
+
+    if (!this.nugetKey) {
+      console.log("##[warning]😢 NUGET_KEY not given");
+      return;
     }
 
-    _printErrorAndExit(msg) {
-        console.log(`##[error]😭 ${msg}`)
-        throw new Error(msg)
+    console.log(`NuGet Source: ${this.nugetSource}`);
+
+    fs.readdirSync(".")
+      .filter((fn) => /\.s?nupkg$/.test(fn))
+      .forEach((fn) => fs.unlinkSync(fn));
+
+    if (!this.noBuild) {
+      this._executeInProcess(`dotnet build -c Release ${this.projectFile}`);
     }
 
-    _executeCommand(cmd, options) {
-        console.log(`executing: [${cmd}]`)
+    this._executeInProcess(
+      `dotnet pack ${
+        this.includeSymbols
+          ? "--include-symbols -p:SymbolPackageFormat=snupkg"
+          : ""
+      } -c Release ${this.projectFile} -o .`
+    );
 
-        const INPUT = cmd.split(" "), TOOL = INPUT[0], ARGS = INPUT.slice(1)
-        return spawnSync(TOOL, ARGS, options)
+    const packages = fs.readdirSync(".").filter((fn) => fn.endsWith("nupkg"));
+    console.log(`Generated Package(s): ${packages.join(", ")}`);
+
+    const pushCmd = `dotnet nuget push *.nupkg -s ${
+        this.nugetSource
+      }/v3/index.json -k ${this.nugetKey} --skip-duplicate${
+        !this.includeSymbols ? " -n" : ""
+      }`,
+      pushOutput = this._executeCommand(pushCmd, { encoding: "utf-8" }).stdout;
+
+    console.log(pushOutput);
+
+    if (/error/.test(pushOutput))
+      this._printErrorAndExit(`${/error.*/.exec(pushOutput)[0]}`);
+
+    const packageFilename = packages.filter((p) => p.endsWith(".nupkg"))[0],
+      symbolsFilename = packages.filter((p) => p.endsWith(".snupkg"))[0];
+
+    process.stdout.write(
+      `::set-output name=PACKAGE_NAME::${packageFilename}` + os.EOL
+    );
+    process.stdout.write(
+      `::set-output name=PACKAGE_PATH::${path.resolve(packageFilename)}` +
+        os.EOL
+    );
+
+    if (symbolsFilename) {
+      process.stdout.write(
+        `::set-output name=SYMBOLS_PACKAGE_NAME::${symbolsFilename}` + os.EOL
+      );
+      process.stdout.write(
+        `::set-output name=SYMBOLS_PACKAGE_PATH::${path.resolve(
+          symbolsFilename
+        )}` + os.EOL
+      );
     }
 
-    _executeInProcess(cmd) {
-        this._executeCommand(cmd, { encoding: "utf-8", stdio: [process.stdin, process.stdout, process.stderr] })
+    if (this.tagCommit) this._tagCommit(version);
+  }
+
+  _checkForUpdate() {
+    if (!this.packageName) {
+      this.packageName = path
+        .basename(this.projectFile)
+        .split(".")
+        .slice(0, -1)
+        .join(".");
     }
 
-    _tagCommit(version) {
-        const TAG = this.tagFormat.replace("*", version)
+    console.log(`Package Name: ${this.packageName}`);
 
-        console.log(`✨ creating new tag ${TAG}`)
+    let url = `${this.nugetSource}/${
+      this.nugetV3FlatContainer
+    }/${this.packageName.toLowerCase()}/index.json`;
+    console.log(`Getting versions from ${url}`);
+    https
+      .get(url, (res) => {
+        let body = "";
 
-        this._executeInProcess(`git tag ${TAG}`)
-        this._executeInProcess(`git push origin ${TAG}`)
-
-        process.stdout.write(`::set-output name=VERSION::${TAG}` + os.EOL)
-    }
-
-    _pushPackage(version, name) {
-        console.log(`✨ found new version (${version}) of ${name}`)
-
-        if (!this.nugetKey) {
-            console.log("##[warning]😢 NUGET_KEY not given")
-            return
+        if (res.statusCode == 404) {
+          console.log("404 response, assuming new package");
+          this._pushPackage(this.version, this.packageName);
         }
 
-        console.log(`NuGet Source: ${this.nugetSource}`)
-
-        fs.readdirSync(".").filter(fn => /\.s?nupkg$/.test(fn)).forEach(fn => fs.unlinkSync(fn))
-
-        if (!this.noBuild) {
-            this._executeInProcess(`dotnet build -c Release ${this.projectFile}`)
+        if (res.statusCode == 200) {
+          res.setEncoding("utf8");
+          res.on("data", (chunk) => (body += chunk));
+          res.on("end", () => {
+            const existingVersions = JSON.parse(body);
+            console.log(`Versions retrieved: ${existingVersions.versions}`);
+            if (existingVersions.versions.indexOf(this.version) < 0)
+              this._pushPackage(this.version, this.packageName);
+          });
         }
+      })
+      .on("error", (e) => {
+        this._printErrorAndExit(`error: ${e.message}`);
+      });
+  }
 
-        this._executeInProcess(`dotnet pack ${this.includeSymbols ? "--include-symbols -p:SymbolPackageFormat=snupkg" : ""} -c Release ${this.projectFile} -o .`)
+  run() {
+    if (!this.projectFile || !fs.existsSync(this.projectFile))
+      this._printErrorAndExit("project file not found");
 
-        const packages = fs.readdirSync(".").filter(fn => fn.endsWith("nupkg"))
-        console.log(`Generated Package(s): ${packages.join(", ")}`)
+    console.log(`Project Filepath: ${this.projectFile}`);
 
-        const pushCmd = `dotnet nuget push *.nupkg -s ${this.nugetSource}/v3/index.json -k ${this.nugetKey} --skip-duplicate${!this.includeSymbols ? " -n" : ""}`,
-            pushOutput = this._executeCommand(pushCmd, { encoding: "utf-8" }).stdout
+    if (!this.version) {
+      if (
+        this.versionFile !== this.projectFile &&
+        !fs.existsSync(this.versionFile)
+      )
+        this._printErrorAndExit("version file not found");
 
-        console.log(pushOutput)
+      console.log(`Version Filepath: ${this.versionFile}`);
+      console.log(`Version Regex: ${this.versionRegex}`);
 
-        if (/error/.test(pushOutput))
-            this._printErrorAndExit(`${/error.*/.exec(pushOutput)[0]}`)
+      const versionFileContent = fs.readFileSync(this.versionFile, {
+          encoding: "utf-8",
+        }),
+        parsedVersion = this.versionRegex.exec(versionFileContent);
 
-        const packageFilename = packages.filter(p => p.endsWith(".nupkg"))[0],
-            symbolsFilename = packages.filter(p => p.endsWith(".snupkg"))[0]
+      if (!parsedVersion)
+        this._printErrorAndExit("unable to extract version info!");
 
-        process.stdout.write(`::set-output name=PACKAGE_NAME::${packageFilename}` + os.EOL)
-        process.stdout.write(`::set-output name=PACKAGE_PATH::${path.resolve(packageFilename)}` + os.EOL)
-
-        if (symbolsFilename) {
-            process.stdout.write(`::set-output name=SYMBOLS_PACKAGE_NAME::${symbolsFilename}` + os.EOL)
-            process.stdout.write(`::set-output name=SYMBOLS_PACKAGE_PATH::${path.resolve(symbolsFilename)}` + os.EOL)
-        }
-
-        if (this.tagCommit)
-            this._tagCommit(version)
+      this.version = parsedVersion[1];
     }
 
-    _checkForUpdate() {
-        if (!this.packageName) {
-            this.packageName = path.basename(this.projectFile).split(".").slice(0, -1).join(".")
-        }
+    console.log(`Version: ${this.version}`);
 
-        console.log(`Package Name: ${this.packageName}`)
-
-        let url = `${this.nugetSource}/v3-flatcontainer/${this.packageName.toLowerCase()}/index.json`
-        console.log(`Getting versions from ${url}`)
-        https.get(url, res => {
-            let body = ""
-
-            if (res.statusCode == 404) {
-                console.log('404 response, assuming new package')
-                this._pushPackage(this.version, this.packageName)
-            }
-                
-
-            if (res.statusCode == 200) {
-                res.setEncoding("utf8")
-                res.on("data", chunk => body += chunk)
-                res.on("end", () => {
-                    const existingVersions = JSON.parse(body)
-                    console.log(`Versions retrieved: ${existingVersions.versions}`)
-                    if (existingVersions.versions.indexOf(this.version) < 0)
-                        this._pushPackage(this.version, this.packageName)
-                })
-            }
-        }).on("error", e => {
-            this._printErrorAndExit(`error: ${e.message}`)
-        })
-    }
-
-    run() {
-        if (!this.projectFile || !fs.existsSync(this.projectFile))
-            this._printErrorAndExit("project file not found")
-
-        console.log(`Project Filepath: ${this.projectFile}`)
-
-        if (!this.version) {
-            if (this.versionFile !== this.projectFile && !fs.existsSync(this.versionFile))
-                this._printErrorAndExit("version file not found")
-
-            console.log(`Version Filepath: ${this.versionFile}`)
-            console.log(`Version Regex: ${this.versionRegex}`)
-
-            const versionFileContent = fs.readFileSync(this.versionFile, { encoding: "utf-8" }),
-                parsedVersion = this.versionRegex.exec(versionFileContent)
-
-            if (!parsedVersion)
-                this._printErrorAndExit("unable to extract version info!")
-
-            this.version = parsedVersion[1]
-        }
-
-        console.log(`Version: ${this.version}`)
-
-        this._checkForUpdate()
-    }
+    this._checkForUpdate();
+  }
 }
 
-new Action().run()
+new Action().run();
